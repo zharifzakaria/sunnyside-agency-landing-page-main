428bc1f1400fef8ee35a532ec1934a01f5235e11
zharifzakaria <zharifzakaria@gmail.com>
2021-06-23 12:24:48 +0800

    added body and footer

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,11 @@
-console.log("Hello world...");
+// Menu Hamburger Click Event
+const hamburger = document.querySelector('#menu-hamburger')
+hamburger.addEventListener('click', openMenu);
+
+function openMenu() {
+    if(hamburger.classList.contains('open')) {
+        hamburger.classList.remove('open');
+    } else {
+        hamburger.classList.add('open');   
+    }
+}
